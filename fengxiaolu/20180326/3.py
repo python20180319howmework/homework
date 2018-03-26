e1af6735bd69e54bd4db794830ae47f3d74f5762
Your Name <1242050367@qq.com>
2018-03-26 10:45:35 +0800

    zhangzhen

---
+++ fengxiaolu/20180326/3.py
@@ -0,0 +1,5 @@
+s="We are FAMILY"
+print(s.upper())
+print(s.lower())
+print(s.split())
+
---
+++ huangzhaoqian/t4.py
@@ -0,0 +1,5 @@
+n=int(input("please input a num:"))
+if n in range(101):
+	print("你好棒啊！继续努力")
+else:
+	print("乖乖，你咋这么不听话呢")
