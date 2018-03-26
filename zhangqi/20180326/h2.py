0c29e77b358fe8299300a5ee2ed65c9c9d0f9381
wojialaoerhuikongda <1299200073@qq.com>
2018-03-26 20:56:39 +0800

    zuoye

---
+++ zhangqi/20180326/h2.py
@@ -0,0 +1,42 @@
+
+#输入一个三为整型数,判断是否是一个水仙花数
+#计算出有多少各水仙花数
+
+num = int (input("请输入一个三位整型数："))
+a = num % 10
+b = num // 10 % 10
+c = num //100 % 10
+if num == a**3 + b**3 + c**3:
+	print ("%s 是水仙花数" % num)
+else:
+	print("%s 不是水仙花数" % num)
+n = 0
+for i in range (100,1000):
+	a1 = i % 10
+	b1 = i//10 % 10
+	c1 = i //100 % 10
+	if i == a1 ** 3 + b1 ** 3 + c1 ** 3:
+		print(i)
+		n = n + 1
+	else:
+		continue
+print("共有%s 个水仙花数" % n)	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
