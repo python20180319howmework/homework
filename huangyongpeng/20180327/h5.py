c0ec4e9f4e1dd3d358daf79964157bc5c243b02f
Your Name <you@example.com>
2018-03-26 20:58:18 +0800

    text

---
+++ huangyongpeng/20180327/h5.py
@@ -0,0 +1,19 @@
+count=0
+count1=0
+count2=0
+
+str0=input("请输入一个字符串：")
+for i in range(len(str0)):
+	if str0[]i.isdigit():
+		count+=1
+	if 65<=ord(str0[i])<=90:
+		count1+=1
+	if 97<=ord(str0[i])<=122:
+		count2+=1
+print(count)
+print(count1)
+print(count2)
+
+
+
+
