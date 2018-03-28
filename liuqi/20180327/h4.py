95836eca5415ce980c1208e0f22bf8f113baef00
liuqi <1875991105@qq.comliuqi>
2018-03-28 08:57:04 +0800

    zuoye

--- liuqi/20180327/h4.py
+++ liuqi/20180327/h4.py
@@ -4,6 +4,7 @@
 year =int(input("请输入年份："))
 month = int(input("请输入月份："))
 day = int(input("请输入日子:"))
+"""
 mysum = day
 days = [31,28,31,30,31,30,31,31,30,31,30,31]
 i=0
@@ -15,4 +16,19 @@ if 0 < month <13 and 0 <= day <= 31:
 		i += 1
 	print("这一天是{}年的第{}天".format(year,mysum))
 else:
-	print("输入错误！")  
+	print("输入错误！") 
+"""
+sumdays = day
+for m in range(1,month):
+	if m==1 or m==3 or m==5 or m==7 or m==8 or m==10 or m==12:
+		sumdays += 31 
+	elif m==4 or m==6 or m==9 or m==11:
+		sumdays += 30
+	elif m==2:
+		if (year%100 != 0 and year%4 == 0) or year%400 == 0:
+			sumdays += 29
+		else:
+			sumdays += 28
+	else:
+		pass
+print("{}年{}月{}日是{}年的第{}天".format(year,month,day,year,sumdays))
---
+++ liuqi/20180328/h8.py
@@ -0,0 +1,8 @@
+"""
+8.删除元祖中的所有重复的元素，并生成一个列表
+"""
+tuple1 = (1,1,3,8,4,0,1,3,3,2) 
+s = set(tuple1)
+#print(s)
+list1 = list(s)
+print(list1)
