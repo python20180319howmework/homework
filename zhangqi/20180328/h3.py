9df5a8d547b02ad3bbcb0c81bdb303720dd94019
wojialaoerhuikongda <1299200073@qq.com>
2018-03-28 20:11:58 +0800

    zuoye

--- zhangqi/20180328/h3.py
+++ zhangqi/20180328/h3.py
@@ -1,10 +1,11 @@
 
 #3.定义一个函数，计算给定的整型数是否为质数
 def num(a):
-	if a % 2 != 0:
-		print("是质数")
-	else:
-		print("不是质数")
+	for i in range(2,a//2+1):
+		if a % i == 0:
+	
+			return print("不是质数")
+	print("是质数")
 b = eval(input("请输入一个整型数："))
 num(b)
 	
