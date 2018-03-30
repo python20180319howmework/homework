89c3b7da4056841c8a6877f171ddabb2bc253677
Tammyqian <1594937607@qq.com>
2018-03-30 09:33:15 +0800

    zuoye

--- huangchaoqian/20180329/t3.py
+++ huangchaoqian/20180329/t3.py
@@ -3,11 +3,16 @@
    1)此函数可以传递任意一组键值对，函数将所有的键存放到一个列表中，返回
    2) 升级以上函数，使得用户必须有的键值为：name:"xxx"   city:"xxxx"
 '''
-def dic(name,*n):
-	dict[name]=
-
-if __name__=="__main__":
+def dic(**kw):
+	print("kw",kw)
+	l=[]
+	for i in kw.keys():
+		l.append(i)
+	print(l)
 	
-
+if __name__=="__main__":
+#	name=input("name:")
+	dic(name="huang",city="hubei")
+	
 
 
