e33ff0f18505a5482ed08416e125e2cf6389a5f7
liuqi <1875991105@qq.comliuqi>
2018-04-03 09:52:24 +0800

    a

--- liuqi/20180402/h1.py
+++ liuqi/20180402/h1.py
@@ -2,5 +2,17 @@
 1. 如果有两个字符串"hello" 和 “world”，
    生成一个列表，列表中元素["hw", "eo", "lr"]
 """
-l = [i+j for i in "hello"for j in "world"]
+l = [i+j for i,j in zip("hello","world")]
 print(l)
+
+
+print("---------------------------")
+l1 = [i for i in "hello"]
+l2 = [j for j in "world"]
+l3 =  []
+for i in range(0,len(l1)):
+	l3.append(l1[i]+l2[i])
+
+print(l3)
+
+
