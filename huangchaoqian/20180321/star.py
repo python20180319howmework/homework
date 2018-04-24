341ff9d354d7171e392c7043dbc70f91ee9684be
Tammyqian <1594937607@qq.com>
2018-04-24 09:56:42 +0800

    zuoye

---
+++ huangchaoqian/20180321/star.py
@@ -0,0 +1,13 @@
+import turtle
+import math
+turtle.color("orange","red")
+turtle.begin_fill()
+turtle.circle(120,180)
+turtle.left(45)
+turtle.forward(240*math.sqrt(2))
+turtle.left(90)
+turtle.forward(240*math.sqrt(2))
+turtle.left(45)
+turtle.circle(120,180)
+turtle.end_fill()
+turtle.done()
